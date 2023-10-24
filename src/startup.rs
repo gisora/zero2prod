382d7bb6f33f03b7a04c47fb38606bad4b78cb08
Giedrius Išora <g.isora@outlook.com>
2023-10-24 22:14:21 +0300

    before 4_telementry chapter

--- src/startup.rs
+++ src/startup.rs
@@ -1,16 +1,17 @@
-use actix_web::{web, App, HttpServer};
-use actix_web::dev::Server;
-use std::net::TcpListener;
-use sqlx::PgConnection;
 use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
+use std::net::TcpListener;
 
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    let db_pool = web::Data::new(db_pool);
 
-pub fn run(listener: TcpListener, connection: PgConnection23) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new( || {
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            app.dataconec
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
